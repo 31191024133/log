70d757560a70a328bb8dedadb08b56998423c54d
Dong Trieu <trieu.ng63@gmail.com>
2022-03-18 14:36:20 +0700

    hh

---
+++ app/build/generated/source/buildConfig/debug/dongtrieu/ueh/loginandregister/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package dongtrieu.ueh.loginandregister;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "dongtrieu.ueh.loginandregister";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
